7ce02bd7d4d343bc2f98988a77cf9477277a6996
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-09-21 20:33:46 +0200

    Updated with ng-show

--- js/app.js
+++ js/app.js
@@ -3,17 +3,21 @@ var app = angular.module('universityApp', [ ]);
 app.controller('teacherCtrl', function($scope){
   $scope.teacher = teacherData;
 	$scope.editing = {};
+	$scope.showBox = false;
 	
 	$scope.editTeacher = function(){
 		angular.copy( $scope.teacher, $scope.editing );
+		$scope.showBox = true;
 	}
 
 	$scope.saveChanges = function() {
 		angular.copy( $scope.editing, $scope.teacher );
+		$scope.showBox = false;
 	}
 
 	$scope.cancelChanges = function() {
 		$scope.editing = {};
+		$scope.showBox = false;
 	}
 });
 
